f985374650af03c17a8e9efd192db15026d2c93c
Park Sungwoo <1998psw@korea.ac.kr>
2022-10-16 00:30:48 +0900

    asdf

--- first_app/src/clock_proj/clock.jsx
+++ first_app/src/clock_proj/clock.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Clock(props) {
-    return ( 
-        <div>
-            <h1>안녕, 리액트!</h1>
-            <h2>현재시간 : {new Date().toLocaleTimeString()}</h2>
-        </div>
-    )
+function Clock(props){
+    return(
+
+            <div id="clock-header">
+                <h1 className="neonText">안녕, 리액트!</h1>
+                <h2 className="neonText">현재 시간 : {new Date().toLocaleTimeString()} </h2>
+            </div>
+    );
 }
 
 export default Clock;
--- first_app/src/index.js
+++ first_app/src/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import reportWebVitals from './reportWebVitals';
-import Library from './first_proj/Library';
+import Clock from './Test01/Clock';
+import './Test01/neonStyle.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+// 기본으로 작성되는 부분 주석처리
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Clock />
+//   </React.StrictMode>
+// );
+
+setInterval( () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+    <Clock/>
+  </React.StrictMode>,
+  document.getElementById('root')
+  )
+}, 1000);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
